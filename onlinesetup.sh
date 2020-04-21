9940b6c458bc36f51e091033d26f58fbfb7d9f0c
Herbert de Oliveira <beto@webjump.com.br>
2020-04-21 02:05:28 -0300

    updating script

--- onlinesetup.sh
+++ onlinesetup.sh
@@ -5,7 +5,7 @@ set -x
 sudo dpkg --configure -a
 sudo apt update
 
-sudo apt install python3 python3-pip nautilus-dropbox docker.io docker-compose git git-flow build-essential libssl-dev flatpak gnome-software-plugin-flatpak vim -y
+sudo apt install python3 python3-pip nautilus-dropbox docker.io docker-compose git git-flow build-essential libssl-dev flatpak gnome-software-plugin-flatpak vim virtualbox ubuntu-restricted-extras -y --force-yes
 
 sudo snap install slack --classic &&  
 sudo snap install skype --classic &&  
