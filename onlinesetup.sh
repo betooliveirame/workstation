dd61ff7a797f076ec95de601e5604b7b5edecb27
Herbert de Oliveira <beto@webjump.com.br>
2020-04-21 04:08:25 -0300

    updating script

--- onlinesetup.sh
+++ onlinesetup.sh
@@ -2,8 +2,8 @@
 
 set -x
 
-sudo dpkg --configure -a  &&
-sudo rm /var/lib/dpkg/lock-frontend; sudo rm /var/cache/apt/archives/lock;  &&
+sudo dpkg --configure -a
+sudo rm /var/lib/dpkg/lock-frontend; sudo rm /var/cache/apt/archives/lock;
 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'  &&
 sudo wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -  &&
