a0d89745d947071d291bd09484c7996bd20e33de
Herbert de Oliveira <beto@webjump.com.br>
2020-04-21 02:40:36 -0300

    updating script

--- onlinesetup.sh
+++ onlinesetup.sh
@@ -2,17 +2,14 @@
 
 set -x
 
-# Basic configuration
-
 sudo dpkg --configure -a
 sudo rm /var/lib/dpkg/lock-frontend; sudo rm /var/cache/apt/archives/lock;
 sudo apt update
 
-# install basic packges
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 
-sudo env ACCEPT_EULA=Y apt install python3 python3-pip nautilus-dropbox docker.io docker-compose git git-flow build-essential libssl-dev flatpak gnome-software-plugin-flatpak vim virtualbox ubuntu-restricted-extras -y
-
-# install applications
+sudo env ACCEPT_EULA=Y apt install python3 python3-pip nautilus-dropbox docker.io docker-compose git git-flow build-essential libssl-dev flatpak gnome-software-plugin-flatpak vim virtualbox ubuntu-restricted-extras google-chrome-stable -y
 
 sudo snap install slack --classic &&  
 sudo snap install skype --classic &&  
@@ -28,23 +25,9 @@ sudo snap install whatsdesk &&
 sudo snap install wps-office-multilang && 
 sudo snap install mailspring
 
-# install packes not found on snap
-
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && 
-sudo flatpak install flathub io.dbeaver.DBeaverCommunity -y &&
-sudo flatpak install flathub com.github.alecaddd.sequeler -y
-
-# install downloadable applications
-
-cd ~/Downloads/ && wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i *.deb
-
-# configure docker without sudo
-
 sudo groupadd docker
 sudo gpasswd -a $USER docker
 sudo service docker.io restart
-
-# update the system
 
 sudo apt update && sudo apt dist-upgrade -y && sudo apt autoclean -y && sudo apt autoremove -y
 
