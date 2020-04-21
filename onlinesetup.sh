414a19bfa54d7783b8f8a6c79a1a26970678bbcc
Herbert de Oliveira <beto@webjump.com.br>
2020-04-21 01:51:27 -0300

    updating script

--- onlinesetup.sh
+++ onlinesetup.sh
@@ -2,24 +2,9 @@
 
 set -x
 
-echo ""
-echo "Updating....."
-echo "##########################################################################"
-echo ""
-
 sudo apt update
 
-echo ""
-echo "Instaling the basic packages"
-echo "##########################################################################"
-echo ""
-
 sudo apt install python3 python3-pip nautilus-dropbox docker.io docker-compose git git-flow build-essential libssl-dev flatpak gnome-software-plugin-flatpak vim ubuntu-restricted-extras -y
-
-echo ""
-echo "Instaling the snap packages"
-echo "##########################################################################"
-echo ""
 
 sudo snap install slack --classic &&  
 sudo snap install skype --classic &&  
@@ -35,31 +20,12 @@ sudo snap install whatsdesk &&
 sudo snap install wps-office-multilang && 
 sudo snap install mailspring
 
-echo ""
-echo "Instaling the flatpak packages"
-echo "##########################################################################"
-echo ""
-
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && 
 sudo flatpak install flathub io.dbeaver.DBeaverCommunity -y &&
 sudo flatpak install flathub com.github.alecaddd.sequeler -y
 
-echo ""
-echo "Instaling the downloadables packages"
-echo "##########################################################################"
-echo ""
-
 cd ~/Downloads/ && wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i *.deb
-
-echo ""
-echo "System updating"
-echo "##########################################################################"
-echo ""
 
 sudo apt update && sudo apt dist-upgrade -y && sudo apt autoclean -y && sudo apt autoremove -y
 
-echo ""
-echo ""
-echo "##########################################################################"
 echo "DONE!"
-echo "##########################################################################"
