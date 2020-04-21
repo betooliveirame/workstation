e95ba443091a265604bbfde06b0ff54cac9deda7
Herbert de Oliveira <beto@webjump.com.br>
2020-04-21 01:12:06 -0300

    first commit

---
+++ onlinesetup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+## Removendo travas eventuais do apt ##
+
+sudo rm /var/lib/dpkg/lock-frontend ; sudo rm /var/cache/apt/archives/lock ;
+
+## Atualizando o repositório ##
+
+sudo apt update && 
+
+## Instalando pacotes e programas do repositório deb do Ubuntu ##
+
+sudo apt install \
+		python3 \
+		python-pip \
+		nautilus-dropbox \
+		docker \
+		docker-compose \
+		git \
+		git-flow \
+		build-essential \
+		libssl-dev \
+		flatpak \
+		gnome-software-plugin-flatpak \
+		vim \
+		-y &&
+
+## Instalando pacotes Snap ##
+
+sudo snap install slack --classic &&  
+sudo snap install skype --classic &&  
+sudo snap install sublime-text --classic &&  
+sudo snap install sublime-merge --classic &&
+sudo snap install --edge node --classic && 
+sudo snap install insomnia && 
+sudo snap install postman &&
+sudo snap install redis-desktop-manager && 
+sudo snap install spotify &&
+sudo snap install odio &&
+sudo snap install whatsdesk &&
+sudo snap install wps-office-multilang && 
+sudo snap install mailspring &&
+
+## Adicionando repositório Flathub ##
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && 
+
+## Instalando Apps do Flathub ##
+
+sudo flatpak install flathub io.dbeaver.DBeaverCommunity -y &&
+sudo flatpak install flathub com.github.alecaddd.sequeler -y &&
+
+## Softwares que precisam de download externo ##
+
+cd ~/Downloads/ && wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i *.deb &&
+
+# vim bootstrap
+
+curl 'https://vim-bootstrap.com/generate.vim' --data 'editor=vim&langs=javascript&langs=php&langs=html&langs=c' > ~/.vimrc
+
+## Atualização do sistema ##
+
+sudo apt update && sudo apt dist-upgrade -y && sudo apt autoclean -y && sudo apt autoremove -y &&
+
+#Fim do Script ##
+
+echo "Finalizado"
