a454febada1433974f9ede9dab8fd0fce16b4cc9
Herbert de Oliveira (Beto) <beto@webjump.com.br>
2021-05-08 17:20:15 -0300

    update
    update

--- onlinesetup.sh
+++ onlinesetup.sh
@@ -42,7 +42,7 @@ ubuntu-restricted-extras \
 google-chrome-stable \
 awscli -y  &&
 
-sudo gpasswd -a $USER docker  &&
+#sudo gpasswd -a $USER docker  &&
 
 # php 7.3 for magento 2
 sudo apt install -y zip unzip php7.4-cli php7.4-common php7.4-opcache php7.4-xml php7.4-gd php7.4-dev php7.4-mysql php7.4-intl php7.4-mbstring php7.4-bcmath php7.4-json php7.4-soap php7.4-zip php7.4-curl &&
